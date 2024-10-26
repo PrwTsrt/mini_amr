bc46a34706ae7f7f4f9e46a6d6bdff3219424f2b
Peerawich <peerawich.tangsiritham@gmail.com>
2024-10-26 19:22:24 +0700

    fine tune controller

--- amrROS2_ws/src/navigation/launch/navigation.launch.py
+++ amrROS2_ws/src/navigation/launch/navigation.launch.py
@@ -20,27 +20,47 @@ def generate_launch_description():
     slam_toolbox_dir = get_package_share_directory("slam_toolbox")
 
     namespace = LaunchConfiguration('namespace')
+    use_namespace = LaunchConfiguration('use_namespace')
     use_rviz = LaunchConfiguration("rviz")
     use_sim_time = LaunchConfiguration('sim')
     use_slam_tb = LaunchConfiguration('slam_tb')
-    params_file = LaunchConfiguration('params_file')
+    use_mapping = LaunchConfiguration('use_mapping')
+    params_file = LaunchConfiguration('params_file')    
+    slam_params_file = LaunchConfiguration('slam_params_file')
+    mapping_params_file = LaunchConfiguration('mapping_params_file')
     map = LaunchConfiguration("map")
 
     default_map_path = os.path.join(robot_navigation_dir, "maps/Turtlebot_Arena_map.yaml")
     slam_config_path = os.path.join(robot_navigation_dir, "config/slam_localization.yaml")
 
-    params_file_ = ReplaceString(
-            source_file=PathJoinSubstitution([robot_navigation_dir, "config", "navigation.yaml"]),
-            replacements={'<robot_namespace>': ("/",LaunchConfiguration("namespace"))},)    
+    slam_params_file = ReplaceString(
+        source_file=slam_params_file,
+        replacements={'<robot_namespace>': ("/",namespace)},
+        condition=IfCondition(use_namespace))   
+    
+    mapping_params_file = ReplaceString(
+        source_file=mapping_params_file,
+        replacements={'<robot_namespace>': ("/",namespace)},
+        condition=IfCondition(use_namespace))   
+
+    params_file = ReplaceString(
+            source_file=params_file,
+            replacements={'<robot_namespace>': ("/",namespace)},
+            condition=IfCondition(use_namespace))     
     
     declare_namespace_cmd = DeclareLaunchArgument(
         'namespace',
         default_value= [EnvironmentVariable('NAMESPACE')],
         description='prefix for node name')
+    
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='true',
+        description='Whether to apply a namespace to the navigation stack')
 
     declare_map_cmd = DeclareLaunchArgument(
             name='map', 
-            default_value=default_map_path,
+            default_value='/home/smr/maps/test03',
             description='Navigation map path'
         )
     declare_use_sim_time_cmd = DeclareLaunchArgument(
@@ -58,15 +78,38 @@ def generate_launch_description():
             default_value='true',
             description='Using slam toolbox localization'
         )
+    declare_use_mapping_cmd = DeclareLaunchArgument(
+            name='use_mapping', 
+            default_value='false',
+            description='Using slam toolbox mapping'
+        )
     
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
-        default_value= params_file_,
+        default_value= PathJoinSubstitution([robot_navigation_dir, "config", "navigation.yaml"]),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
+    
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value= slam_config_path,
+        description='Full path to the SLAM localization parameters file')
+    
+    declare_mapping_params_file_cmd = DeclareLaunchArgument(
+        'mapping_params_file',
+        default_value=PathJoinSubstitution([robot_navigation_dir, "config", "mapper_params_online_async.yaml"]),
+        description='Full path to the SLAM mapping parameters file')
     
     slam_configured_params = ParameterFile(
         RewrittenYaml(
-            source_file=slam_config_path,
+            source_file=slam_params_file,
+            root_key=namespace,
+            param_rewrites='',
+            convert_types=True),
+        allow_substs=True)
+    
+    mapping_params = ParameterFile(
+        RewrittenYaml(
+            source_file=mapping_params_file,
             root_key=namespace,
             param_rewrites='',
             convert_types=True),
@@ -94,20 +137,39 @@ def generate_launch_description():
     amcl = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(PathJoinSubstitution(
                 [nav2_bringup_dir, 'launch', 'localization_launch.py'])),
-            condition=UnlessCondition(use_slam_tb),
+            condition=IfCondition(
+                PythonExpression(["'", use_mapping, "' == 'false' and'", use_slam_tb, "' == 'false'" ])),
             launch_arguments={
-                'map' : map,
+                'map' : default_map_path,
                 'use_sim_time': use_sim_time,
                 'params_file':  params_file,
             }.items()
         )
-
-    slam_tb_localization = Node(
-        condition=IfCondition(use_slam_tb),
+    
+    slam_tb_mapping = Node(
         parameters=[
-          slam_configured_params,
+          mapping_params,
           {'use_sim_time': use_sim_time}
         ],
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen',
+        condition=IfCondition(use_mapping),
+        remappings=[
+                ("/map", "map"),
+                ("/map_metadata", "map_metadata")]    
+        )
+
+    slam_tb_localization = Node(
+        condition=IfCondition(
+                PythonExpression(["'", use_mapping, "' == 'false' and'", use_slam_tb, "' == 'true'" ])),
+        parameters=[
+          slam_configured_params,
+          {
+            'use_sim_time': use_sim_time,
+            'map_file_name': map
+        }],
         package='slam_toolbox',
         executable='localization_slam_toolbox_node',
         name='slam_toolbox',
@@ -122,6 +184,7 @@ def generate_launch_description():
         PushRosNamespace(namespace),
         SetRemap('/tf','tf'),
         SetRemap('/tf_static','tf_static'),
+        slam_tb_mapping,
         amcl,
         slam_tb_localization,
         navigation,
@@ -132,7 +195,11 @@ def generate_launch_description():
     return LaunchDescription([
         # Declare the launch options
         declare_namespace_cmd,
+        declare_use_namespace_cmd,
+        declare_use_mapping_cmd,
         declare_params_file_cmd,
+        declare_slam_params_file_cmd,
+        declare_mapping_params_file_cmd,
         declare_map_cmd,
         declare_use_sim_time_cmd,
         declare_use_rviz_cmd,
