aba6fa384fc4e5b36842118cf8cebe9a7e9eeef1
PrwTsrt <peerawich.tangsiritham@gmail.com>
2024-12-03 12:49:05 +0700

    Remove install, log, and build directories from tracking

--- amrROS2_ws/build/action_autodock/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/smr/workspaces/amrROS2_ws/install/action_autodock'
--- amrROS2_ws/build/sllidar_ros2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/smr/workspaces/amrROS2_ws/src/sllidar_ros2
-# Build directory: /home/smr/workspaces/amrROS2_ws/build/sllidar_ros2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
